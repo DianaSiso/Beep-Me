37e7ccb43fc9fc26e64b3d5ab77410dbd5d9aa04
AndreiaPp <andreia.portela@ua.pt>
2022-01-16 17:55:20 +0000

    css fixed; added color to late orders

--- Frontend/beep_me_app/src/app/choice/choice.component.ts
+++ Frontend/beep_me_app/src/app/choice/choice.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChoiceComponent implements OnInit {
 
-  constructor() { console.log(localStorage.getItem("restId"));}
+  constructor() { }
 
   ngOnInit(): void {
   }
