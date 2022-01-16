fb02844dc9ccf7b4de9d47924e964957a65dbdc7
Miguel Garcia Marques <61141327+mikemike198@users.noreply.github.com>
2022-01-16 22:08:39 +0000

    added swagger

--- Backend/spring_backend_server/beep-me/src/main/java/server/beep/me/beepme/BeepMeApplication.java
+++ Backend/spring_backend_server/beep-me/src/main/java/server/beep/me/beepme/BeepMeApplication.java
@@ -3,7 +3,10 @@ package server.beep.me.beepme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class BeepMeApplication {
 
 	public static void main(String[] args) {
