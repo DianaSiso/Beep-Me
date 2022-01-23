ff1f6c7488c35659d448c1fb66ebc3835a1ce32e
Miguel Garcia Marques <61141327+mikemike198@users.noreply.github.com>
2022-01-23 17:04:12 +0000

    Datagen container in detached mode

--- Backend/dataGen_server/DataGen/generateAndRunContainer.sh
+++ Backend/dataGen_server/DataGen/generateAndRunContainer.sh
@@ -3,4 +3,4 @@ sudo docker rmi beep-me-data-image
 # sudo ./mvnw clean install
 sudo ./mvnw spring-boot:build-image -Dspring-boot.build-image.imageName=beep-me-data-image
 # sudo docker build -t beep-me-image-datagen .
-sudo docker run -p 9000:9000 --name beep-me-data-container beep-me-data-image #--rest=$1
+sudo docker run -p 9000:9000 -d --name beep-me-data-container beep-me-data-image #--rest=$1
