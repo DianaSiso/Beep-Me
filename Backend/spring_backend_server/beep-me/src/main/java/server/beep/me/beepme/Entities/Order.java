caf6b4c2a445275fbde27a9cc37570766fe8a18a
Miguel Garcia Marques <61141327+mikemike198@users.noreply.github.com>
2022-01-24 17:09:13 +0000

    changed generation of ids

--- Backend/spring_backend_server/beep-me/src/main/java/server/beep/me/beepme/Entities/Order.java
+++ Backend/spring_backend_server/beep-me/src/main/java/server/beep/me/beepme/Entities/Order.java
@@ -24,7 +24,7 @@ public class Order {
 
     @Id
     @Column
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @JsonBackReference
