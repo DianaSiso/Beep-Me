4759d1bc763acf27a8ba58cd30eb9550db8c113a
Miguel Garcia Marques <61141327+mikemike198@users.noreply.github.com>
2022-01-24 20:21:38 +0000

    removed detached mode in generateContainer

--- Frontend/beep_me_app/generateAndRunContainer.sh
+++ Frontend/beep_me_app/generateAndRunContainer.sh
@@ -2,4 +2,4 @@ sudo docker stop frontend_container
 sudo docker rm frontend_container
 sudo docker rmi frontend
 sudo docker build -t frontend .
-sudo docker run -d -p 80:80 --name frontend_container frontend
+sudo docker run -p 80:80 --name frontend_container frontend
